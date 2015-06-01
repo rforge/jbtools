9ceda5379f5f1cbfb4bf43e87e68bc88475ecb3d
jbuttlar <jbuttlar@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-06-01 10:53:23 +0000

    updated usage of suggested package doMC
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/jbtools@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/JBTools/R/registerParallel.R
+++ pkg/JBTools/R/registerParallel.R
@@ -1,7 +1,7 @@
 registerParallel <- function(
 ##title<< Set up a parallel computing front end
  pckg.parallel = 'doMC'  ##<< character string: Package to use for parallel
-                         ##   computing. Has to be (for the time beeing) one of doMC or doParallel.
+                         ##   computing. Has to be (for the time beeing) one of 'doMC' or 'doParallel'.
  , max.cores = 0         ##<< integer: amount of cores to use
 )
   ##description<< This function automatically sets up a cluster in a consistent way
@@ -12,23 +12,27 @@ registerParallel <- function(
   ##seealso<< 
   ##\code{\link[foreach]{foreach}}, \code{\link[doMC]{registerDoMC}}
 {
-  w <- NULL
-  if (max.cores == 0) 
-    max.cores   <- detectCores()
-  if (max.cores == 1 || getDoParWorkers() < max.cores) {
-    cat(paste('Registering ', max.cores, ' cores.\n', sep = ''))
-    if (pckg.parallel == 'doMC'){
-      require(doMC)
-      w <- max.cores
-      registerDoMC(w)
-    } else if (pckg.parallel == 'doParallel'){
-      w <- makeCluster(max.cores)
-    } else if (pckg.parallel == 'snow') {
-      stop('Do not use this function to create snow clusters. Use sfInit from package snowfall instead!')
-    } else {
-      stop(paste('Package ', pckg.parallel, ' is not (yet) supported!', sep=''))
+    w <- NULL
+    if (max.cores == 0) 
+        max.cores   <- detectCores()
+    if (max.cores == 1 || getDoParWorkers() < max.cores) {
+        cat(paste('Registering ', max.cores, ' cores.\n', sep = ''))
+        if (pckg.parallel == 'doMC'){
+            if (requireNamespace("doMC", quietly = TRUE)) {                
+                w <- max.cores
+                doMC::registerDoMC(w)                
+            } else {
+                stop(paste("Package 'doMC' does not seem to be available on this",
+                           "platform. Please set argument 'pckg.parallel' to 'doParralel'"), sep = "")
+            }         
+        } else if (pckg.parallel == 'doParallel'){
+            w <- makeCluster(max.cores)
+        } else if (pckg.parallel == 'snow') {
+            stop('Do not use this function to create snow clusters. Use sfInit from package snowfall instead!')
+        } else {
+            stop(paste('Package ', pckg.parallel, ' is not (yet) supported!', sep=''))
+        }
     }
-  }
-  ##value<< For "doMC": the amount of cores and for doParallel: the cluster object created. 
-  invisible(w)
+    ##value<< For 'doMC': the amount of cores and for doParallel: the cluster object created. 
+    invisible(w)
 }
